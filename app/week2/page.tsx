942110b20b0d3b78c3aeecac1bfdf269cc416461
Juno <anint3647@gmail.com>
2024-09-27 02:40:18 +0900

    Group Modifiers

--- app/week2/page.tsx
+++ app/week2/page.tsx
@@ -1,23 +1,25 @@
 export default function Login() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
-      <div className="p-5 w-3/5 max-w-screen-sm flex flex-col gap-2">
+    <main className="bg-gray-100 grid grid-cols-3 w-full h-screen p-5 ">
+      <div className="flex flex-col">
         <div>img</div>
-        <input
-          className="w-full rounded-full py-3 h-10 border-gray-200 border-2 pl-5 outline-none ring ring-transparent focus:ring-gray-200 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
-          type="email"
-          placeholder="Email"
-        />
-        <input
-          className="w-full rounded-full py-3 h-10 border-gray-200 border-2 pl-5 outline-none ring ring-transparent focus:ring-gray-200 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
-          type="text"
-          placeholder="Username"
-        />
-        <input
-          className="w-full rounded-full py-3 h-10 border-gray-200 border-2 pl-5 outline-none ring ring-transparent focus:ring-gray-200 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
-          type="password"
-          placeholder="Password"
-        />
+        <div>
+          <input
+            className=" rounded-full py-3 h-10 border-gray-200 border-2 pl-5 outline-none ring ring-transparent focus:ring-gray-200 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            className=" rounded-full py-3 h-10 border-gray-200 border-2 pl-5 outline-none ring ring-transparent focus:ring-gray-200 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            className=" rounded-full py-3 h-10 border-gray-200 border-2 pl-5 outline-none ring ring-transparent focus:ring-gray-200 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
         <button className="bg-gray-200 text-black py-2 rounded-full hover:bg-gray-300 active:scale-90 transition-transform font-semibold outline-none">
           Log in
         </button>
