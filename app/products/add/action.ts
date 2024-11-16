0a0c8539bdd00fb20f975c6698fbc5652a370306
Juno <anint3647@gmail.com>
2024-11-16 19:33:09 +0900

    product upload for appendFile

--- app/products/add/action.ts
+++ app/products/add/action.ts
@@ -1,11 +1,58 @@
 "use server";
 
-export async function uploadProduct(formData: FormData) {
+import { z } from "zod";
+import fs from "fs/promises";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const productSchema = z.object({
+  photo: z
+    .string({
+      required_error: "photo is required",
+    })
+    .min(10)
+    .max(50),
+  title: z.string({ required_error: "title is required" }),
+  description: z.string({ required_error: "description is required" }),
+  price: z.coerce.number({ required_error: "price is required" }),
+});
+
+export async function uploadProduct(_: any, formData: FormData) {
   const data = {
     photo: formData.get("photo"),
     title: formData.get("title"),
     price: formData.get("price"),
     description: formData.get("description"),
   };
-  console.log(data);
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+    data.photo = `/${data.photo.name}`;
+  }
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: result.data.photo,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+    }
+  }
 }
