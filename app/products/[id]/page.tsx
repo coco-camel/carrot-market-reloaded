c9d08f5adc808654c14886325bd2bf7afe51e67b
Juno <anint3647@gmail.com>
2024-11-20 02:36:33 +0900

    Variants re commit

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -48,7 +48,11 @@ export default async function ProductDetail({
   return (
     <div>
       <div className="relative aspect-square">
-        <Image fill src={`${product.photo}/public`} alt={product.title} />
+        <Image
+          fill
+          src={`${product.photo}/width=500,height=500`}
+          alt={product.title}
+        />
       </div>
       <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
         <div className="size-10 overflow-hidden rounded-full">
